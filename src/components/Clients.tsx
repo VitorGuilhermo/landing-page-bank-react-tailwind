e3b0e7071188b369ee988743147314e1f574b277
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-03-08 20:53:25 -0300

    add: Testimonials sections and components

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { airbnb, binance, coinbase, dropbox } from '../assets';
+import styles from '../styles/styles';
+
+interface ClientsData {
+    id: string
+    logo: string
+}
+
+const clients: ClientsData[] = [
+    {
+        id: 'client-1',
+        logo: airbnb,
+    },
+    {
+        id: 'client-2',
+        logo: binance,
+    },
+    {
+        id: 'client-3',
+        logo: coinbase,
+    },
+    {
+        id: 'client-4',
+        logo: dropbox,
+    },
+];
 
 export function Clients() {
     return (
-        <div>
-            Clients
-        </div>
+        <section className={`${styles.flexCenter} my-4`}>
+            <div className={`${styles.flexCenter} flex-wrap w-full`}>
+                {
+                    clients.map((client) => (
+                        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
+                            <img src={client.logo} alt='client_logo' className='sm:w-[192px] w-[100px] object-contain' />
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
     )
 }
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import styles from "../styles/styles"
+import { people01, people02, people03 } from "../assets";
+
+import { FeedbackCard } from "./FeedbackCard"
+
+interface FeedbackData {
+    id: string
+    content: string
+    name: string
+    title: string
+    img: string
+}
+
+const feedback: FeedbackData[] = [
+    {
+        id: "feedback-1",
+        content:
+            "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+        name: "Herman Jensen",
+        title: "Founder & Leader",
+        img: people01,
+    },
+    {
+        id: "feedback-2",
+        content:
+            "Money makes your life easier. If you're lucky to have it, you're lucky.",
+        name: "Steve Mark",
+        title: "Founder & Leader",
+        img: people02,
+    },
+    {
+        id: "feedback-3",
+        content:
+            "It is usually people in the money business, finance, and international trade that are really rich.",
+        name: "Kenn Gallagher",
+        title: "Founder & Leader",
+        img: people03,
+    },
+];
 
 export function Testimonials() {
     return (
-        <div>
-            Testimonials
-        </div>
+        <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`} >
+            <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
+
+            <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+                <h1 className={styles.heading2}>
+                    What people are <br className='sm:block hidden' />  saying about us
+                </h1>
+                <div className='w-full md:mt-0 mt-6'>
+                    <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+                        Everything you need to accept card payments and grow your business anywhere on the planet.
+                    </p>
+                </div>
+            </div>
+
+            <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]' >
+                {
+                    feedback.map((card) => (
+                        <FeedbackCard key={card.id} {...card} />
+                    ))
+                }
+            </div>
+        </section>
     )
 }
